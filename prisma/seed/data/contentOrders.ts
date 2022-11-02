d32eae16e3a9b0c182d4b5453d266ed512345577
willen <willen.sesantos@gmail.com>
2022-11-02 15:45:41 -0300

    chore: Altera id que relaciona ordem ao produto

--- prisma/seed/data/contentOrders.ts
+++ prisma/seed/data/contentOrders.ts
@@ -3,37 +3,37 @@ import { PrismaClient } from "@prisma/client";
 export const contentOrdersToInsert = [
   {
     pizza_name: "Marguerita",
-    order_id: 11,
+    order_id: 1,
     quantidade: 1,
   },
   {
     pizza_name: "Muçarela",
-    order_id: 11,
+    order_id: 2,
     quantidade: 2,
   },
   {
     pizza_name: "Portuguesa",
-    order_id: 12,
+    order_id: 1,
     quantidade: 1,
   },
   {
     pizza_name: "Napolitana",
-    order_id: 13,
+    order_id: 3,
     quantidade: 3,
   },
   {
     pizza_name: "Frango com catupiry",
-    order_id: 13,
+    order_id: 4,
     quantidade: 1,
   },
   {
     pizza_name: "Napolitana",
-    order_id: 14,
+    order_id: 3,
     quantidade: 2,
   },
   {
     pizza_name: "Romeu e julieta",
-    order_id: 14,
+    order_id: 2,
     quantidade: 1,
   },
 ];
