b1a945c9739ceaaa196c212b332e54c5c5ff5740
willen <willen.sesantos@gmail.com>
2022-11-02 15:57:10 -0300

    feat: Cria rota principal

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,22 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { OrdersController } from "../controllers/ordersController";
 import { PizzariaController } from "../controllers/pizzariaController";
 
 const router = Router();
 const pizzariaController = new PizzariaController();
 const ordersController = new OrdersController();
+
+//home
+router.get("/", (_: Request, res: Response) => {
+  res.status(200).json({
+    "Listar pizzas": "GET - https://pizzaria-api.onrender.com/api/pizzas",
+    "Listar pedidos": "GET - https://pizzaria-api.onrender.com/api/order",
+    "Listar pedidos por id":
+      "GET - https://pizzaria-api.onrender.com/api/order/:id",
+    "Cadastrat novo pedido":
+      "POST - https://pizzaria-api.onrender.com/api/order",
+  });
+});
 
 //listar pizzas
 router.get("/api/pizzas", pizzariaController.listPizzas);
